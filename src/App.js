e94d6b2051a352a6cbbc2a2a6342fa8fb36310cf
Jon Hayden <barefootjon@gmail.com>
2020-02-15 07:28:12 -0700

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          goodbye.
+          My new text.
         </p>
         <a
           className="App-link"
